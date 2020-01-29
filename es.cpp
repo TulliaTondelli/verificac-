dd6db8b85b9dfc593908a4dd934563f2330b3fa2
TulliaTondelli <tullytondelli15gmail.com>
2020-01-29 22:04:01 +0100

    verifica base

---
+++ es.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+	freopen("./input/1.txt", "r", stdin);
+	freopen("./output/1.txt", "w", stdout);
+	
+	int n;
+	cin >> n;
+	int v[n];
+	
+	for(int i=0; i<n; i++){
+		cin >> v[i];
+	}
+    
+    for(int i=0; i<n; i++){
+    	
+	}
+	cout << " " << endl;	
+}
